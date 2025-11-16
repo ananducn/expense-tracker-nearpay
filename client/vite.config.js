f967712e2f920b4e696d7c069c20b25d0eeda09e
Ananducn <anandu.cn112@gmail.com>
2025-11-17 01:31:15 +0530

    render touches

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwind from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "",
   plugins: [react(), tailwind()],
-  server: { port: 3000 },
 });
